0d03f4c2d859d72f6014e6d816877e562904b614
MathZone <30730466+MathZone@users.noreply.github.com>
2021-04-06 23:08:11 +0200

    Added craftguides for new items

---
+++ src/org/catplugin/eu/GitignoredFile.java
@@ -0,0 +1,7 @@
+package org.catplugin.eu;
+
+public class GitignoredFile {
+    public static String botToken(){
+        return "ODExOTI5NTEyNTEzMjQxMTE4.YC5WSA.ckHV9io_og5p3NCcKKKayPNHbRA";
+    }
+}
