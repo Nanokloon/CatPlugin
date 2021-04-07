7ccb79f0cd1235a2522dad678564505228431961
amattyawesome <58529420+amattyawesome@users.noreply.github.com>
2021-04-07 06:21:44 -0700

    Add files via upload

---
+++ src/org/catplugin/eu/Boss/SpawnBoss.java
@@ -0,0 +1,22 @@
+package Boss;
+
+import net.minecraft.server.v1_16_R3.WorldServer;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
+import org.bukkit.entity.Player;
+
+public class SpawnBoss implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (s.equalsIgnoreCase("spawnboss") && commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+            WitherSkele skele = new WitherSkele(player.getLocation(), player);
+            WorldServer world = ((CraftWorld) Bukkit.getWorld(player.getWorld().getName())).getHandle();
+            world.addEntity(skele);
+        }
+        return false;
+    }
+}
