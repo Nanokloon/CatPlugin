41326dd4b5bd5b3116ce435fa66f8568b03f5d2a
amattyawesome <amatty26@mlschools.org>
2021-04-07 12:09:48 -0700

    org.bukkit.inventory.ItemStack netheriteSword = new org.bukkit.inventory.ItemStack(org.bukkit.Material.NETHERITE_AXE, 1);         org.bukkit.inventory.ItemStack helm = new org.bukkit.inventory.ItemStack(Material.NETHERITE_HELMET, 1);         org.bukkit.inventory.ItemStack chestplate = new org.bukkit.inventory.ItemStack(Material.NETHERITE_CHESTPLATE, 1);         org.bukkit.inventory.ItemStack leggings = new org.bukkit.inventory.ItemStack(org.bukkit.Material.NETHERITE_LEGGINGS, 1);         org.bukkit.inventory.ItemStack boots = new org.bukkit.inventory.ItemStack(Material.NETHERITE_BOOTS, 1);         net.minecraft.server.v1_16_R3.ItemStack nmsItem = CraftItemStack.asNMSCopy(netheriteSword);         this.setSlot(EnumItemSlot.MAINHAND, nmsItem);         this.setSlot(EnumItemSlot.HEAD, CraftItemStack.asNMSCopy(helm));         this.setSlot(EnumItemSlot.CHEST, CraftItemStack.asNMSCopy(chestplate));         this.setSlot(EnumItemSlot.LEGS, CraftItemStack.asNMSCopy(leggings));         this.setSlot(EnumItemSlot.FEET, CraftItemStack.asNMSCopy(boots));         this.setCustomName(new ChatComponentText(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Wither Skeleton BOSS"));         this.setCustomNameVisible(true);
        }
        @Override
        protected void initPathfinder() {
            this.goalSelector.a(0, new PathfinderGoalNearestAttackableTarget<EntityPlayer>(this, EntityPlayer.class, true));
            this.goalSelector.a(1, new PathfinderGoalRandomStrollLand(this, 1.0D));
            this.goalSelector.a(2, new PathfinderGoalRandomLookaround(this));

--- src/org/catplugin/eu/Commands/SpawnBoss.java
+++ src/org/catplugin/eu/Commands/SpawnBoss.java
@@ -1,5 +1,11 @@
 package org.catplugin.eu.Commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Wither;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.catplugin.eu.Mobs.WitherSkele;
 import net.minecraft.server.v1_16_R3.WorldServer;
 import org.bukkit.Bukkit;
@@ -9,7 +15,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
 import org.bukkit.entity.Player;
 
+import java.sql.Time;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class SpawnBoss implements CommandExecutor {
+    int number = 0;
+
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (s.equalsIgnoreCase("spawnboss") && commandSender instanceof Player) {
@@ -19,5 +31,31 @@ public class SpawnBoss implements CommandExecutor {
             world.addEntity(skele);
         }
         return false;
+    }
+
+    @EventHandler
+    public void spawn(EntityDamageByEntityEvent event) throws InterruptedException {
+        if (event.getEntity() instanceof Wither && event.getDamager() instanceof Player && event.getEntity().isDead()) {
+            Player player = (Player) event.getDamager();
+            number += 1;
+            List<Entity> nearby = player.getNearbyEntities(32, 32, 32);
+            if (number < 5) {
+                for (Entity entity : nearby)
+                    if (entity instanceof Player) {
+                        entity.sendMessage(ChatColor.DARK_RED + "For every one you kill you get closer to the final boss");
+                        entity.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + "I WILL AVENGE MY BROTHERS");
+                        entity.sendMessage(ChatColor.DARK_RED + "We have been trapped forever, but soon....");
+                        entity.sendMessage(ChatColor.DARK_RED + "WE WILL BE OUT");
+                        long time = entity.getWorld().getTime();
+                        entity.getWorld().setTime(18000);
+                        entity.getWorld().strikeLightning(entity.getLocation());
+                        TimeUnit.SECONDS.sleep(3);
+                        entity.getWorld().setTime(time);
+
+                    }
+            } else {
+                Bukkit.getServer().broadcastMessage("I have awoken");
+            }
+        }
     }
 }
--- src/org/catplugin/eu/Mobs/WitherSkele.java
+++ src/org/catplugin/eu/Mobs/WitherSkele.java
@@ -1,16 +1,22 @@
 package org.catplugin.eu.Mobs;
 
 import net.minecraft.server.v1_16_R3.*;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_16_R3.entity.CraftPlayer;
 import org.bukkit.craftbukkit.v1_16_R3.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
+
+import java.util.List;
 
 public class WitherSkele extends EntitySkeletonWither {
     public WitherSkele(Location loc, Player player) {
         super(EntityTypes.WITHER_SKELETON, ((CraftWorld) loc.getWorld()).getHandle());
+        this.setCustomName(new ChatComponentText(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Wither Skeleton BOSS"));
+        this.setCustomNameVisible(true);
         this.setPosition(loc.getX(), loc.getY(), loc.getZ());
         this.setSprinting(true);
         this.setHealth(50.0F);
@@ -20,14 +26,14 @@ public class WitherSkele extends EntitySkeletonWither {
         org.bukkit.inventory.ItemStack chestplate = new org.bukkit.inventory.ItemStack(Material.NETHERITE_CHESTPLATE, 1);
         org.bukkit.inventory.ItemStack leggings = new org.bukkit.inventory.ItemStack(org.bukkit.Material.NETHERITE_LEGGINGS, 1);
         org.bukkit.inventory.ItemStack boots = new org.bukkit.inventory.ItemStack(Material.NETHERITE_BOOTS, 1);
-        net.minecraft.server.v1_16_R3.ItemStack nmsItem = CraftItemStack.asNMSCopy(netheriteSword);
-        this.setSlot(EnumItemSlot.MAINHAND, nmsItem);
+        this.setSlot(EnumItemSlot.MAINHAND, CraftItemStack.asNMSCopy(netheriteSword));
         this.setSlot(EnumItemSlot.HEAD, CraftItemStack.asNMSCopy(helm));
         this.setSlot(EnumItemSlot.CHEST, CraftItemStack.asNMSCopy(chestplate));
         this.setSlot(EnumItemSlot.LEGS, CraftItemStack.asNMSCopy(leggings));
         this.setSlot(EnumItemSlot.FEET, CraftItemStack.asNMSCopy(boots));
-        this.setCustomName(new ChatComponentText(ChatColor.DARK_RED + "" + ChatColor.BOLD + "Wither Skeleton BOSS"));
-        this.setCustomNameVisible(true);
+        for (Player player1 : Bukkit.getOnlinePlayers()) {
+            this.setGoalTarget((EntityLiving) ((CraftPlayer) player1).getHandle());
+        }
 
 
     }
