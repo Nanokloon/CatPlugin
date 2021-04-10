cecc8826bc5bc9a3da66a048beaae992462d3766
BestRanger11 <58529420+BestRanger11@users.noreply.github.com>
2021-04-10 08:06:40 -0700

    Update BossFight.java
    fixed mikid

--- src/org/catplugin/eu/Mobs/BossFight.java
+++ src/org/catplugin/eu/Mobs/BossFight.java
@@ -1,75 +1,68 @@
 package org.catplugin.eu.Mobs;
 
 import net.minecraft.server.v1_16_R3.WorldServer;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.*;
 import org.bukkit.craftbukkit.v1_16_R3.CraftWorld;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.WitherSkeleton;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.catplugin.eu.Main;
 
+import java.util.List;
+import java.util.Random;
+
 public class BossFight implements Listener {
+    private final Main main;
+    public static WorldServer worldServer;
+    public BossFight(Main main) {
+        this.main = main;
+    }
+
 
     @EventHandler
     public void onBossSpawn(EntitySpawnEvent e) {
-        //Bukkit.broadcastMessage("spawn event");
-        if (e.getEntity() instanceof WitherSkeleton) {
-            if (ChatColor.stripColor(e.getEntity().getName()).equals("Wither Skeleton BOSS")) {
-                WitherSkeleton witherSkeleton = (WitherSkeleton) e.getEntity();
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("CatPlugin"), new Runnable() {
-                    @Override
-                    public void run() {
-                        witherSkeleton.setHealth(0);
+        if (ChatColor.stripColor(e.getEntity().getName()).equals("Wither Skeleton BOSS")) {
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
+                @Override
+                public void run() {
+                    if (!e.getEntity().isDead()) {
+                        Location loc = e.getLocation();
+                        NetheriteDefender defender = new NetheriteDefender(loc);
+                        worldServer.addEntity(defender);
+
+
                     }
-                }, 6000);
+                }
+            }, 99L);
+            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
+                @Override
+                public void run() {
+                    List<Entity> nearby = e.getEntity().getNearbyEntities(32, 32, 32);
+                    for (Entity tmp : nearby) {
+                        if (tmp instanceof Player) {
+                            Random ra = new Random();
+                            if (ra.nextInt(100) >= 50) {
+                                Location location = tmp.getLocation();
+                                TNTPrimed tnt = (TNTPrimed) e.getEntity().getWorld().spawnEntity(location, EntityType.PRIMED_TNT);
+                                tnt.setFuseTicks(20);
 
-
-                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("CatPlugin"), new Runnable() {
-                    @Override
-                    public void run() {
-                        if (!e.getEntity().isDead()) {
-                            Location loc = e.getLocation();
-                            WitherAttacker witherAttacker = new WitherAttacker(loc);
-                            //WorldServer world = Main.worldServer;
-                            Main.worldServer.addEntity(witherAttacker);
-                            runAgain(e);
-
+                            }
                         }
                     }
-                }, 100);
-            }
+                }
+            }, 200);
         }
     }
 
-    @EventHandler
-    public void runAgain(EntitySpawnEvent e){
-        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Bukkit.getServer().getPluginManager().getPlugin("CatPlugin"), new Runnable() {
-            @Override
-            public void run() {
-                if (!e.getEntity().isDead()) {
-                    Location loc = e.getLocation();
-                    WitherAttacker defender = new WitherAttacker(loc);
-                    //WorldServer world = Main.worldServer;
-                    Main.worldServer.addEntity(defender);
-                    runAgain(e);
-                }
-            }
-        }, 100);
-    }
-    @EventHandler
-    public void onDamage (EntityDamageByEntityEvent e){
-        Player player = (Player) e.getDamager();
-           if (e.getEntity() instanceof WitherSkele) {
-               if (((WitherSkele) e.getEntity()).getMaxHealth() / 2 > ((WitherSkele) e.getEntity()).getHealth()) {
-                   Mikid m = new Mikid();
-                   m.spawn(player);
-                   Bukkit.getServer().broadcastMessage("MiKid2015 has spawned at" + main.x + ", " + main.y + ", " + main.y);
+        @EventHandler
+        public void onDamage (EntityDamageByEntityEvent e){
+            Player player = (Player) e.getDamager();
+            if (ChatColor.stripColor(e.getEntity().getName()).equals("Wither Skeleton BOSS")) {
+                if (((WitherSkele) e.getEntity()).getMaxHealth() / 2 > ((WitherSkele) e.getEntity()).getHealth()) {
+                    Mikid.spawn(player);
                 }
 
                 Random rnd = new Random();
@@ -81,4 +74,3 @@ public class BossFight implements Listener {
             }
         }
     }
-
